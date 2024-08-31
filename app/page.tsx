9c811f1539f1a9faa5cb6849d655a7e3687e17b7
sahil <sahilmulani501@gmail.com>
2024-08-31 20:25:22 +0530

    update client

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
           <h3 className="text-3xl font-semibold">
             Ask Find-X AI about{" "}
             <a
-              href="https://supergood.ai/"
+              href="https://docs.supergood.ai/"
               className="underline text-[#ff371a]"
             >
               Supergood.ai
